c4225d47d4c1931f92834ed88676304f37fab08e
Elian Oviedo <elianoviedo2905@gmail.com>
2021-10-25 20:59:37 -0300

    se agrego ClasePrueba

---
+++ ClasePrueba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerPractica
+{
+    class ClasePrueba
+    {
+        public string Rueda { get; set; }
+        public string Puertas { get; set; }
+
+        public void Claseprueba(string rueda,string puertas)
+        {
+            rueda = Rueda;
+            puertas = Puertas;
+        }
+    }
+}
